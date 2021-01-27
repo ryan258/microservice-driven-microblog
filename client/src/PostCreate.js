870f7be660f6099c2c3782d2edfbbfc1240ab215
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-01-27 14:05:37 -0600

    Build React Client app to submit posts. Apply CORS to services.

---
+++ client/src/PostCreate.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import axios from "axios"
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("")
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    // post to our Posts service
+    await axios.post("http://localhost:4000/posts", {
+      title
+    })
+
+    setTitle("")
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          {/* use 2 way property binding */}
+          <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default PostCreate
--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const { randomBytes } = require("crypto")
+const cors = require("cors")
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 // we'll store all our pposts in this obj
 const posts = {}
